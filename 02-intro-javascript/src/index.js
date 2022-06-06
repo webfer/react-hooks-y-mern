fc3ed0810b5a975aab675709fa48a03c58260a53
‘webfer’ <mx7.bcn@pm.me>
2022-06-05 19:02:51 -0700

    feat: update index js

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,18 @@
 // file of work
+
+const active = true;
+
+// let msg = '';
+//
+// if (active) {
+//   msg = 'active';
+// } else {
+//   msg = 'Unactive';
+// }
+
+//const msg = active ? 'Active' : 'unactive';
+//const msg = active ? 'Active' : null;
+
+//const msg = active && 'Active';
+const msg = active && 'Active';
+console.log(msg);
