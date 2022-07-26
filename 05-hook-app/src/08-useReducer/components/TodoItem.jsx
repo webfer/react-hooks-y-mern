a6c573ef654f2f6f76fc93cafc768305aebb80fd
‘webfer’ <mx7.bcn@pm.me>
2022-07-25 21:19:49 -0700

    feat: eccion 10: Profundizando Hooks - useReducer

---
+++ 05-hook-app/src/08-useReducer/components/TodoItem.jsx
@@ -0,0 +1,19 @@
+export const TodoItem = ({ todo, onDeleteTodo, onToggleTodo }) => {
+  return (
+    <li className="list-group-item d-flex justify-content-between">
+      <span
+        onClick={() => {
+          onToggleTodo(todo.id);
+        }}
+        className={`align-self-center ${
+          todo.done ? 'text-decoration-line-through' : ''
+        }`}
+      >
+        {todo.description}
+      </span>
+      <button onClick={() => onDeleteTodo(todo.id)} className="btn btn-danger">
+        Remove
+      </button>
+    </li>
+  );
+};
---
+++ 05-hook-app/src/08-useReducer/hooks/useTodos.js
@@ -0,0 +1,43 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../todoReducer';
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: '[TODO] Add todo',
+      payload: todo,
+    };
+    dispatch(action);
+  };
+
+  const handleRemoveTodo = (id) => {
+    dispatch({
+      type: '[TODO] Remove todo',
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: '[TODO] Toggle todo',
+      payload: id,
+    });
+  };
+
+  return {
+    todos,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+    todosCount: todos.length,
+    handleNewTodo,
+    handleRemoveTodo,
+    handleToggleTodo,
+  };
+};
